519064dc1acc0898b71880021e3d7caecc6f167c
Richard <rlister@norbar.com>
2023-11-09 16:26:04 +0000

    added cycle club application using delegates

---
+++ ClubMembershipApplication/ClubMembershipApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClubMembershipApplication.Views;
+namespace ClubMembershipApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IView mainView = Factory.GetMainViewObject();
+			mainView.RunView();
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ ClubMembershipApplication/ClubMembershipApplication/Views/MainView.cs
@@ -0,0 +1,58 @@
+﻿using ClubMembershipApplication.FieldValidators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Views
+{
+	internal class MainView : IView
+	{
+		public IFieldValidator FieldValidator => null;
+
+		IView _registerView = null;
+		IView _loginView = null;
+
+		public MainView(IView registerView, IView loginView) 
+		{
+			_registerView = registerView;
+			_loginView = loginView;
+		}
+
+		public void RunView()
+		{
+			CommonOutputText.WriteMainHeading();
+
+			Console.WriteLine("Please press 'L' to login, or press 'R' to register");
+
+			ConsoleKey key = Console.ReadKey().Key;
+
+			if (key == ConsoleKey.L)
+			{
+				RunUserLoginView();
+			}
+			else if (key == ConsoleKey.R)
+			{
+				RunUserRegistrationView();
+				RunUserLoginView();
+			}
+			else
+			{
+				Console.Clear();
+				Console.WriteLine("Goodbye");
+				Console.ReadKey();
+			}
+		}
+
+		private void RunUserRegistrationView()
+		{
+			_registerView.RunView();
+		}
+
+		private void RunUserLoginView()
+		{
+			_loginView.RunView();
+		}
+	}
+}
